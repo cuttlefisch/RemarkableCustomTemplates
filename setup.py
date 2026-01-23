ca4d278df5fcca7f03e7f9fe13d0e23a48a48f03
cuttlefisch <hayden@cuttle.codes>
2026-01-23 01:27:46 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name="remarkablecustomtemplates",
+    version="0.0.1",
+    description="read, preview, and write custom Remarkable native json templates.",
+    author="cuttlefisch",
+    author_email="",
+    packages=["customtemplates"],
+    package_dir={"customtemplates": "src/customtemplates"},
+    package_data={"customtemplates": ["data/*.template", "data/*.json"]},
+    test_suite="nose.collector",
+    tests_require=["nose==1.3.7"],
+    python_requires=">=3.14",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
